26f3717ff8b7baed1ef617517ffee9442d13bc8a
Denis Ahmatovic <37641046+Andosius@users.noreply.github.com>
2022-09-21 16:13:36 +0200

    move constant value CLIENT_PLATFORM to cfg

--- cfg/config.go
+++ cfg/config.go
@@ -3,4 +3,5 @@ package cfg
 const (
 	GET_PLAYER_PREFERENCES = "https://playerpreferences.riotgames.com/playerPref/v3/getPreference/Ares.PlayerSettings"
 	PUT_PLAYER_PREFERENCES = "https://playerpreferences.riotgames.com/playerPref/v3/savePreference"
+	CLIENT_PLATFORM = "ew0KCSJwbGF0Zm9ybVR5cGUiOiAiUEMiLA0KCSJwbGF0Zm9ybU9TIjogIldpbmRvd3MiLA0KCSJwbGF0Zm9ybU9TVmVyc2lvbiI6ICIxMC4wLjE5MDQyLjEuMjU2LjY0Yml0IiwNCgkicGxhdGZvcm1DaGlwc2V0IjogIlVua25vd24iDQp9"
 )
